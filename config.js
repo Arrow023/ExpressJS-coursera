1d235c1dd91222e625c048f6fbfb587bb47bda63
arrow023 <piyushchohan48@gmail.com>
2020-06-12 08:16:18 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 module.exports = {
     'secretKey':'12345-67890-98765-43210',
     'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook':{
+        clientId:'2570762503172379',
+        clientSecret:'2a7d5533b2d9c0e572141dff71296f40'
+    }
     
 }
